4289ecb97afdbf92108e58387234e51ee002a61e
ValoriomMX <76182918+ValoriomMX@users.noreply.github.com>
2020-12-25 21:34:14 -0600

    Add files via upload

---
+++ backend.js
@@ -0,0 +1,33 @@
+function banUser() {
+  document.getElementById('launchButton').style.backgroundColor = " rgba(252, 17, 0, 0.753)";
+  document.getElementById('launchButton').innerHTML = "Banned";
+  alert("You are banned from Aero Client. Appeal at https://aeroclient.net/appeal");
+  console.log("[ACP]: ACP_LAUNCH_BANNED");
+}
+
+function startupChecks() {
+  document.getElementById('launchButton').innerHTML = "Initializing ACP";
+  document.getElementById('launchButton').style.backgroundColor = " rgba(109, 0, 252, 0.671)";
+  bannedCheck()
+}
+
+function notWhitelisted() {
+  document.getElementById('launchButton').style.backgroundColor = " rgba(252, 17, 0, 0.753)";
+  document.getElementById('launchButton').innerHTML = "Unauthorized";
+  alert("You are not authorized to launch Aero Client at this time.");
+  console.log("[ACP]: Not whitelisted!");
+}
+
+function checkBranch2() {
+
+  var branch2 = document.querySelector('.branch-button2').innerHTML
+  var branch3 = document.querySelector('.branch-button3').innerHTML
+
+ if(branch2 == "✔️Beta") {
+    console.log("[Aero Client] Checked Beta Branch");
+    launchClient2();
+  } else if(branch3 == "✔️Dev") {
+    console.log("[Aero Client] Checked Dev Branch");
+    launchClient3();
+  }
+}
---
+++ bans.js
@@ -0,0 +1,16 @@
+function bannedCheck() {
+  fs.readFile(getAppDataPath('.minecraft/launcher_accounts.json'), function (err, data) {
+    if (err) throw err;
+    if(data.indexOf('5ca59f07ff7542ddbf7c0e92309d8dc2') >= 1) {
+     banUser()
+    } else if(data.indexOf('4fb9e313df1a41c6929ca3c066869e01') >= 1) {
+     banUser()
+    } else if(data.indexOf('c01ee440a8084d69b700635842e9bdc6') >= 1) {
+     banUser()
+    } else if(data.indexOf('81703177e5cb4256b6729098541407f8') >= 1) {
+     banUser()
+    } else {
+      checkBranch()
+    }
+  });
+}
